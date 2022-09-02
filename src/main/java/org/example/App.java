dac7689e7b0b19dc4323fc2e220ce4789a66be80
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-03 00:19:32 +0700

    Homework 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,37 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+//TODO Создать репозиторий maxima-java-01
+//+
+//TODO Создать maven-проект maxima-java-01
+//+
+
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("I'm high!");
-    }
+
+        //TODO Описать две переменные одного типа и поменять местами их значения.
+
+        int a = 1024;
+        int b = 64;
+
+        //Выводим для проверки
+        System.out.println( "Начальное значение переменных:" );
+        System.out.println( a );
+        System.out.println( b );
+
+        //TODO ... и поменять местами их значения.
+        int subVar = a;
+        a = b;
+        b = subVar;
+
+        //Выводим для проверки
+        System.out.println( "Результирующее значение переменных:" );
+        System.out.println( a );
+        System.out.println( b );
+
+
+        }
 }
+//TODO Выгрузить результаты в репозиторий
