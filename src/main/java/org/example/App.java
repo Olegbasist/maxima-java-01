15a4c5fae15993459582ee30d22c3e25d83f4e23
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-03 00:46:59 +0700

    Homework 1 Изменил вывод на "дружелюбный" )))

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,8 @@
 package org.example;
 
 //TODO Создать репозиторий maxima-java-01
-//+
+//https://github.com/Olegbasist/maxima-java-01.git
+
 //TODO Создать maven-проект maxima-java-01
 //+
 
@@ -17,21 +18,28 @@ public class App
         int b = 64;
 
         //Выводим для проверки
-        System.out.println( "Начальное значение переменных:" );
-        System.out.println( a );
-        System.out.println( b );
+        String strOfA = String.valueOf(a);
+        String strOfB = String.valueOf(b);
+        System.out.println( "Начальное значение переменной 'a':" + strOfA);
+        System.out.println( "а начальное значение переменной 'b':" + strOfB);
+
 
         //TODO ... и поменять местами их значения.
+        System.out.println();
+        System.out.println( "Секундочку, происходит магия ..." + " кручу-верчу, перепутать хочу!" );
         int subVar = a;
         a = b;
         b = subVar;
 
         //Выводим для проверки
-        System.out.println( "Конечное значение переменных:" );
-        System.out.println( a );
-        System.out.println( b );
+        strOfA = String.valueOf(a);
+        strOfB = String.valueOf(b);
+        System.out.println();
+        System.out.println( "Теперь значение переменной 'a':" + strOfA);
+        System.out.println( "а значение переменной 'b':" + strOfB);
 
 
         }
 }
 //TODO Выгрузить результаты в репозиторий
+//+
