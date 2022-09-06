f16bd31f47f6226885a35ec00d444d76c149387c
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-07 00:19:08 +0700

    Homework 2 Заработало!

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,17 +4,39 @@ package org.example;
 // возвращающую N-й член последовательности Фибоначчи.
 // Мы будем считать что 0 — это нулевой член последовательности, 1 — первый, и т.д.
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
 
     public static void main( String[] args ) {
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Говорите точно сколько вешать в байтах: ");
+        byte b = in.nextByte();
+        System.out.println(b);
 
-        //TODO Описать две переменные одного типа и поменять местами их значения.
+        System.out.println(faktorial(b));
+        */
+        Scanner in = new Scanner(System.in);
+        System.out.println("Говорите точно сколько фибаначить в байтах: ");
+        byte n = in.nextByte();
+        System.out.println(n);
 
+        System.out.println(fibonazzi(n));
 
+    }
 
+    /*public static int faktorial(int b){
+        return b == 0 ? 0 : b == 1 ? 1 : b*faktorial(b-1);
+    }*/
 
-        }
+    public static long fibonazzi(byte n) {
+        return n == 0 ? 0
+                : n == 1 ? 1
+                : fibonazzi((byte) (n-1)) + fibonazzi((byte) (n-2));
+
+    }
 }
 //TODO Выгрузить результаты в репозиторий
 //+
