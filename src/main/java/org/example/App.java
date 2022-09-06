749407f57d6bb4622cf4cfef1bfc19f4eb858e27
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-07 00:30:56 +0700

    Homework 2 Подчистил. Работает на натуральных числах до n=(???)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,26 +10,15 @@ public class App
 {
 
     public static void main( String[] args ) {
-        /*
-        Scanner in = new Scanner(System.in);
-        System.out.println("Говорите точно сколько вешать в байтах: ");
-        byte b = in.nextByte();
-        System.out.println(b);
 
-        System.out.println(faktorial(b));
-        */
         Scanner in = new Scanner(System.in);
-        System.out.println("Говорите точно сколько фибаначить в байтах: ");
+        System.out.println("Говорите точно сколько фибаначить: ");
         byte n = in.nextByte();
-        System.out.println(n);
 
-        System.out.println(fibonazzi(n));
+        System.out.println("Вот сколько мы нафибаначили: ");
+        System.out.print(fibonazzi(n));
 
     }
-
-    /*public static int faktorial(int b){
-        return b == 0 ? 0 : b == 1 ? 1 : b*faktorial(b-1);
-    }*/
 
     public static long fibonazzi(byte n) {
         return n == 0 ? 0
@@ -38,5 +27,3 @@ public class App
 
     }
 }
-//TODO Выгрузить результаты в репозиторий
-//+
