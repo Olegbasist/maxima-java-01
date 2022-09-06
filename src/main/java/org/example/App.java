c2419a055cc980b3968599df248c820ccce348e6
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-06 21:15:40 +0700

    Homework 2 Пустой проект для Фибоначчи

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,42 +1,17 @@
 package org.example;
 
-//TODO Создать репозиторий maxima-java-01
-//https://github.com/Olegbasist/maxima-java-01.git
-
-//TODO Создать maven-проект maxima-java-01
-//+
+//TODO Написать рекурсивную функцию long fibonazzi (byte N),
+// возвращающую N-й член последовательности Фибоначчи.
+// Мы будем считать что 0 — это нулевой член последовательности, 1 — первый, и т.д.
 
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
 
         //TODO Описать две переменные одного типа и поменять местами их значения.
 
-        int a = 1024;
-        int b = 64;
 
-        //Выводим для проверки
-        String strOfA = String.valueOf(a);
-        String strOfB = String.valueOf(b);
-        System.out.println( "Начальное значение переменной 'a':" + strOfA);
-        System.out.println( "а начальное значение переменной 'b':" + strOfB);
-
-
-        //TODO ... и поменять местами их значения.
-        System.out.println();
-        System.out.println( "Секундочку, происходит магия ..." + " кручу-верчу, перепутать хочу!" );
-        int subVar = a;
-        a = b;
-        b = subVar;
-
-        //Выводим для проверки
-        strOfA = String.valueOf(a);
-        strOfB = String.valueOf(b);
-        System.out.println();
-        System.out.println( "Теперь значение переменной 'a':" + strOfA);
-        System.out.println( "а значение переменной 'b':" + strOfB);
 
 
         }
