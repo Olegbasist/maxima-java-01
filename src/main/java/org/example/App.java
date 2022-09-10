510df583d34810f382dab5ebe08f3bb4e0a892a5
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-10 19:41:11 +0700

    Homework 4.1 Вычисление дней в месяце работает. Ура.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,7 @@
 package org.example;
 
-//TODO Написать рекурсивную функцию long fibonazzi (byte N),
-// возвращающую N-й член последовательности Фибоначчи.
-// Мы будем считать что 0 — это нулевой член последовательности, 1 — первый, и т.д.
+//TODO Написать функцию byte daysCount(byte month, int year),
+// которая по номеру месяца и номеру года возвращает число дней в месяце.
 
 import java.util.Scanner;
 
@@ -12,16 +11,114 @@ public class App
     public static void main( String[] args ) {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Говорите точно сколько фибаначить: ");
-        byte N = in.nextByte();
+        System.out.println("Выберите год:");
+        int year = in.nextInt();
 
-        System.out.println("Вот сколько мы нафибаначили: ");
-        System.out.print(fibonazzi(N));
+        System.out.println("А теперь любой из 12 месяцев:");
+        byte month = in.nextByte();
+        in.close();
+
+        int days = daysCount(month, year);
+        String monthName = monthName(month);
+        System.out.println("В " +monthName+ " " +year+ "-го года: " +days+ " дней" );
+
+        String yearType = yearType(year);
+        System.out.println(yearType);
+    }
+    public static byte daysCount(byte month, int year){
+        byte days = -1;
+        switch (month) {
+
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            case 2:
+                if (year % 400 == 0
+                        || year % 100 != 0 && year % 4 == 0)
+                    days = 29;
+                else
+                    days = 28;
+                break;
+            default:
+
+        }
+        return days;
 
     }
 
-    public static long fibonazzi(byte N) {
-        return N == 0 || N == 1 ? N : fibonazzi((byte) (N-1)) + fibonazzi((byte) (N-2));
+    public static String yearType (int year) {
+        String yearType;
+
+        if (year % 400 == 0
+                || year % 100 != 0 && year % 4 == 0) {
+                    yearType = "Это високосный год";
+        }
+            else {
+                yearType = "Это обычный невисокосный год";
+            }
+
+        return yearType;
 
     }
+
+    public static String monthName (byte month){
+        String monthName;
+        switch (month){
+            case 1:
+                monthName = "январе";
+                break;
+            case 2:
+                monthName = "феврале";
+                break;
+            case 3:
+                monthName = "марте";
+                break;
+            case 4:
+                monthName = "апреле";
+                break;
+            case 5:
+                monthName = "мае";
+                break;
+            case 6:
+                monthName = "июне";
+                break;
+            case 7:
+                monthName = "июле";
+                break;
+            case 8:
+                monthName = "августе";
+                break;
+            case 9:
+                monthName = "сентябре";
+                break;
+            case 10:
+                monthName = "октябре";
+                break;
+            case 11:
+                monthName = "ноябре";
+                break;
+            case 12:
+                monthName = "декабре";
+                break;
+            default:
+                monthName = "небритябре???";
+
+
+        }
+        return monthName;
+
+    }
+
 }
