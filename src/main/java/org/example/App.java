a6c13235adc0804df35f7b294b75cc485807fa51
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-25 18:45:54 +0700

    HomeWork 4. Пока не работает

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,20 +9,88 @@ public class App {
 
     public static void main(String[] args) {
 
- // Выбор числа для проверки на простоту и вывод результата
+ // Задаем и проверяем массив
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Выберите число: ");
+        System.out.println("Насколько всё длинно? ");
         int[] arr = new int[in.nextInt()];
+
+        System.out.println("Посчитаемся от 1 до " + arr.length);
+
+
+// Выводим результаты
+
         System.out.println(maxDigitsSumPosition(arr));
+        //System.out.println(sumDigits(84));
+
+
+    }
+// Ищем наибольшую сумму
+    public static int maxDigitsSumPosition(int[] arr) {
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+// Заполняем и проверяем, что элементы массива заполнились значениями
+            arr[i] = i+1;
+            //System.out.println(arr[i]);
+// Считаем
+
+            if (arr[i] < 10) {
+               sum = arr[i];
+            }
+            else {
+               int array = arr[i];
+               while (array != 0) {
+                   sum += (array % 10);
+                   array /= 10;
+               }
+
+            }
+
+            //return arr[i] >= 10 ? arr[i] % 10 + maxDigitsSumPosition(i / 10) : arr[i];
+
+            //(arr[i] % 10 + summ(arr[i] / 10)) ;
+                    //> summ ? summ : summ = arr[i] % 10 + summ(arr[i] / 10);
+        }
+        /*while (i != arr.length){
+            //summ = i % 10 + summ(i / 10);
+            i++;
+            return i;
+        }*/
+        return sum;
 
 
     }
 
-    public static byte maxDigitsSumPosition(int[] arr) {
-
-
-        return 0;
-    }
 
 }
+/////////////////////////////////////////////////////////////////////
+/*зад2=щщёт циклом 🔧
+ int summacifr == 0;
+ int cifra ;
+int ostatok ;
+
+    for (int i == 10;   i < n*10 ;     i == i*10 ; )
+{
+       ostatok == n % i  ;
+       cifra ==  ostatok*10 / i ;
+       summacifr == summacifr + cifra ;
+  n == n - ostatok; //занулитьЗАДНИЕцифры
+}
+ ,,,,дальше СРАВНИВАТЬ  summacifr
+//==makssumma
+,,,,,,,,,,,,,,,,return { ВСЕиндексы с makssumma }*/
+
+////////////////////////////////////////////////////////////////////
+
+    /*public static long sumDigits(long i) {
+        return i == 0 ? 0 : i % 10 + sumDigits(i / 10);
+    }*/
+
+//////////////////////////////////////////////////////////////////////
+// Перебираем массив длинной (?)
+    // Суммируем цифры
+        // Если очередная сумма цифр больше (вводим переменную) "Сумма", то обновляем ее и продолжаем
+        // Если очередная сумма цифр меньше или равна, то просто продолжаем
+    // Возвращаем "Сумму"
