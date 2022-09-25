fd945d179e1b8c307aa066ce9859ef7cf572af55
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-25 15:16:29 +0700

    HomeWork 4. Начало

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,9 @@
 package org.example;
 
-// 08.09.2022 Написать функцию int isSimple(int n), которая определяет является ли число простым
-//  — возвращает 0, если число простое и минимальный делитель, если число не является простым.
+// TODO: 08.09.2022 Написать функцию byte maxDigitsSumPosition(int[] arr),
+//  которая вернет наибольший индекс элемента, имеющего максимальную сумму цифр.
 
-import java.math.BigInteger;
 import java.util.Scanner;
-import static java.lang.Math.sqrt;
 
 public class App {
 
@@ -14,44 +12,17 @@ public class App {
  // Выбор числа для проверки на простоту и вывод результата
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Выберите число:");
-        int n = in.nextInt();
-        System.out.println(isPrime(n));
-        if (isSimple(n) == 0) {
-            System.out.println("Число делиться только на 1 и на само себя");
-        }
-        else {
-        System.out.println("Минимальный делитель: " + isSimple(n));
-        }
-
+        System.out.print("Выберите число: ");
+        int[] arr = new int[in.nextInt()];
+        System.out.println(maxDigitsSumPosition(arr));
 
 
     }
-// Проверка на простоту с помощью стандартного класса BigInteger
-        public static String isPrime(int n){
-            BigInteger bigInteger = BigInteger.valueOf(n);
-            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(n));
-            return probablePrime ? "Это простое число!" : "Это как-бы даже и совсем не простое... , но и не совсем сложное... так сказать.";
+
+    public static byte maxDigitsSumPosition(int[] arr) {
 
 
-    }
-// Вычисляем минимальный делитель
-    public static int isSimple(int n){
-
-        if (n <= 2){
-            return 0;
-        }
-
-        for (int i = 2; i < sqrt(n)+1; i++){
-            if (n % i == 0){
-
-            return i;
-
-            }
-
-        }
         return 0;
-
     }
 
 }
