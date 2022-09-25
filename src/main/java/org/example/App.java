341970261b11d36365fdd50ff8783f06ca57fbc1
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-25 13:00:51 +0700

    HomeWork 4. Работает

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ package org.example;
 
 import java.math.BigInteger;
 import java.util.Scanner;
-
+import static java.lang.Math.sqrt;
 import static java.math.BigInteger.probablePrime;
 
 public class App {
@@ -20,6 +20,12 @@ public class App {
         System.out.println("Выберите число:");
         int n = in.nextInt();
         System.out.println(isPrime(n));
+        if (isSimple(n) == 0) {
+            System.out.println("Минимальный делитель: 1");
+        }
+        else {
+        System.out.println("Минимальный делитель: " + isSimple(n));
+        }
 
 
 
@@ -28,8 +34,22 @@ public class App {
         public static String isPrime(int n){
             BigInteger bigInteger = BigInteger.valueOf(n);
             boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(n));
-            return probablePrime ? "Это простое число!" : "Это как-бы даже и совсем не простое ... так сказать.";
+            return probablePrime ? "Это простое число!" : "Это как-бы даже и совсем не простое... , но и не совсем сложное... так сказать.";
 
+
+    }
+// Вычисляем минимальный делитель
+    public static int isSimple(int n){
+
+        for (int i = 2; i < sqrt(n)+1; i++){
+            if (n % i == 0){
+
+            return i;
+
+            }
+
+        }
+        return 0;
 
     }
 
