a797bbc0b1915696c62bc03579b185419a390a5d
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-25 11:18:07 +0700

    HomeWork 4. Сделал проверку на простоту встроенным классом.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,134 +1,70 @@
 package org.example;
 
-//TODO Написать функцию byte daysCount(byte month, int year),
-// которая по номеру месяца и номеру года возвращает число дней в месяце.
+// TODO: 25.09.2022 Написать функцию int isSimple(int n), которая определяет является ли число простым
+//  — возвращает 0, если число простое и минимальный делитель, если число не является простым.
+//
+//Число является простым, если оно делится без остатка только на 1 и на само себя.
 
+import java.math.BigInteger;
 import java.util.Scanner;
+
+import static java.math.BigInteger.probablePrime;
 
 public class App {
 
     public static void main(String[] args) {
 
+ // Выбор числа для проверки на простоту и вывод результата
+
         Scanner in = new Scanner(System.in);
-        System.out.println("Выберите год:");
-        int year = in.nextInt();
+        System.out.println("Выберите число:");
+        int n = in.nextInt();
+        System.out.println(isPrime(n));
 
-        System.out.println("А теперь любой из 12 месяцев:");
-        byte month = in.nextByte();
-        in.close();
 
-        int days = daysCount(month, year);
-        String monthName = monthName(month);
-        System.out.println("В " + monthName + " " + year + "-го года: " + days + " дней");
 
-        String yearType = yearType(year);
-        System.out.println(yearType);
     }
+// Проверка на простоту с помощью стандартного класса BigInteger
+        public static String isPrime(int n){
+            BigInteger bigInteger = BigInteger.valueOf(n);
+            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(n));
+            return probablePrime ? "Это простое число!" : "Это как-бы даже и совсем не простое ... так сказать.";
 
-    public static byte daysCount(byte month, int year) {
-        byte days = -1;
-        switch (month) {
-
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                days = 31;
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                days = 30;
-                break;
-            case 2:
-                days = (byte) (year % 400 == 0
-                                                                        || year % 100 != 0 && year % 4 == 0
-                                                                        ? 29
-                                                                        : 28);
-                /*if (year % 400 == 0
-                        || year % 100 != 0 && year % 4 == 0)
-                    days = 29;
-                else
-                    days = 28;*/
-                break;
-            default:
-
-        }
-        return days;
 
     }
 
-    public static String yearType(int year) {
-        String yearType;
 
-        if (year % 400 == 0
-                || year % 100 != 0 && year % 4 == 0) {
-            if (year > 2022)
-                yearType = "Это будет високосный год";
+    /*
+    * package javaapplication4.simple;
+
+/**
+ *
+ * @author V5
+    public class JavaApplication4Simple {
+
+
+        public static void main(String[] args) {
+
+            // функцию int isSimple(int n)
+            int n; int k == 2; double ostatok ==0;
+            int isSimple;
+
+            if (k > n/2)  print "0";
             else
-                yearType = "Это был високосный год";
-        } else {
-            if (year > 2022)
-                yearType = "Это будет обычный невисокосный год";
-            else
-                yearType = "Это был обычный невисокосный год";
-        }
+            if  (n % k > 0 ); (ostatok == n % k ;
+            print "k";  print "ostatok"; )
+    else k == k+1;
+    goto 22   //лучше цикл--ВСПОМНИТЬ
 
 
-        return yearType;
-    }
 
 
-    public static String monthName (byte month){
-        String monthName;
-        switch (month){
-            case 1:
-                monthName = "январе";
-                break;
-            case 2:
-                monthName = "феврале";
-                break;
-            case 3:
-                monthName = "марте";
-                break;
-            case 4:
-                monthName = "апреле";
-                break;
-            case 5:
-                monthName = "мае";
-                break;
-            case 6:
-                monthName = "июне";
-                break;
-            case 7:
-                monthName = "июле";
-                break;
-            case 8:
-                monthName = "августе";
-                break;
-            case 9:
-                monthName = "сентябре";
-                break;
-            case 10:
-                monthName = "октябре";
-                break;
-            case 11:
-                monthName = "ноябре";
-                break;
-            case 12:
-                monthName = "декабре";
-                break;
-            default:
-                monthName = "небритябре???";
 
 
         }
-        return monthName;
 
     }
+
+     */
 
 }
