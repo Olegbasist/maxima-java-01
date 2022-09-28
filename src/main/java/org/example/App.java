d571deb63d7ab8582ef98796f2487468d719f162
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-09-28 11:46:28 +0700

    HomeWork 4. Поправил проблему первых 10 ячеек.
    + Убрал 3 пересчёта суммы для вывода в консоль. Теперь считает 1 раз + 1 раз пересчитывает сумму

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ package org.example;
 // 08.09.2022 Написать функцию byte maxDigitsSumPosition(int[] arr),
 //  которая вернет наибольший индекс элемента, имеющего максимальную сумму цифр.
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class App {
 
@@ -12,13 +12,15 @@ public class App {
 
 // Задаем и проверяем массив
 
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
         //System.out.println("Сколько будем считать? ");
-        int[] arr = new int[]{7890,1,23,456,10000,1212121212};
+        int[] arr = new int[]{7890,1,23,456,10000,1212121212,7298390};
 
         //System.out.println("Посчитаемся от 1 до " + arr.length);
-        System.out.println("Максимальное значение будет в ячейке: " +maxDigitsSumPosition(arr));
-        System.out.println("Сумма цифр числа "+arr[maxDigitsSumPosition(arr)]+ " равна " +digitsSum(arr[maxDigitsSumPosition(arr)]) );
+        int i = maxDigitsSumPosition(arr);
+        byte sum = (byte) digitsSum(arr[i]);
+        System.out.println("Максимальное значение будет в ячейке: " +i);
+        System.out.println("Сумма цифр числа "+arr[i]+ " равна " +sum );
 
     }
 
@@ -34,13 +36,14 @@ public class App {
             byte sum = (byte) digitsSum(arr[i]);
 
 // Вывод индекса, значения и суммы цифр
+
             /*System.out.print(i);
             System.out.print(" ");
             System.out.print(arr[i]);
             System.out.print(" ");
             System.out.println(sum);*/
 
-                if (arr[i] < 10) {
+                if (arr[i] < 10 & sum > maximum) {
                     maximum = (byte) arr[i];
                     index = i;
                 }
